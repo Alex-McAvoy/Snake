06560a80e3d25c97fb145de12aba0cd2c59d0ed4
Alex-McAvoy <931772462@qq.com>
2023-01-19 03:53:22 +0800

    Add files via upload

---
+++ src/ts/food.ts
@@ -0,0 +1,29 @@
+//食物类
+class Food {
+    element: HTMLElement; //食物对应元素
+
+    constructor() {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.element = document.getElementById("food")!;
+    }
+
+    //获取食物x轴坐标
+    get X() {
+        return this.element.offsetLeft;
+    }
+
+    //获取食物y轴坐标
+    get Y() {
+        return this.element.offsetTop;
+    }
+
+    //修改食物位置
+    change() {
+        //生成随机位置，食物位置范围0~290，要求%10=0
+        const top = Math.round(Math.random() * 29) * 10;
+        const left = Math.round(Math.random() * 29) * 10;
+
+        this.element.style.left = left + "px";
+        this.element.style.top = top + "px";
+    }
+}
